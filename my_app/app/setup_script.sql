7d788e715263d559065c150dc0cdc8e4f3f0f3c0
James Weakley <james.weakley@omnata.com>
2024-11-19 21:17:32 +0000

    Python tracing test

--- my_app/app/setup_script.sql
+++ my_app/app/setup_script.sql
@@ -6,6 +6,33 @@
 -- Extension Code
 -- Refer to https://docs.snowflake.com/en/developer-guide/native-apps/creating-setup-script for a detailed understanding of this file.
 
-CREATE OR ALTER VERSIONED SCHEMA core;
+create application role if not exists APP_CONSUMER;
 
--- The rest of this script is left blank for purposes of your learning and exploration.
+CREATE OR ALTER VERSIONED SCHEMA CORE;
+
+CREATE OR REPLACE FUNCTION CORE.CUSTOM_SPANS_PYTHON_EXAMPLE() RETURNS STRING
+LANGUAGE PYTHON
+RUNTIME_VERSION = 3.9
+PACKAGES = ('snowflake-telemetry-python', 'opentelemetry-api')
+HANDLER = 'custom_spans_function'
+AS $$
+from snowflake import telemetry
+from opentelemetry import trace
+import time
+def custom_spans_function():
+  tracer = trace.get_tracer("my.tracer")
+  with tracer.start_as_current_span("my.span") as span:
+    span.add_event("About to wait", {"seconds": 5})
+    time.sleep(5)
+    span.add_event("About to wait some more", {"seconds": 3})
+    time.sleep(3)
+    span.add_event("Done waiting")
+
+  return "success"
+$$;
+
+grant usage on schema CORE 
+to application role APP_CONSUMER;
+
+grant usage on function CORE.CUSTOM_SPANS_PYTHON_EXAMPLE()
+to application role APP_CONSUMER;
