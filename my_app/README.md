087e0769665d45d89fc331b63d343f08412f2316
James Weakley <james.weakley@omnata.com>
2025-10-31 06:12:48 +0000

    Repro for bound secret permissions issue

--- my_app/README.md
+++ my_app/README.md
@@ -1,13 +1,51 @@
-## Introduction
+## Repro for Snowflake case 01167074
 
-This is the basic project template for a Snowflake Native App project. It contains minimal code meant to help you set up your first application object in your account quickly.
+The issue occurs when a native application provides access to one of its functions via an application role, but the function references a secret which the caller does not have access to.
 
-### Project Structure
-| File Name | Purpose |
-| --------- | ------- |
-| README.md | The current file you are looking at, meant to guide you through a Snowflake Native App project. |
-| app/setup_script.sql | Contains SQL statements that are run when an account installs or upgrades a Snowflake Native App. |
-| app/manifest.yml | Defines properties required by the application package. Find more details at the [Manifest Documentation.](https://docs.snowflake.com/en/developer-guide/native-apps/creating-manifest)
-| app/README.md | Exposed to the account installing the Snowflake Native App with details on what it does and how to use it. |
-| snowflake.yml | Used by the Snowflake CLI tool to discover your project's code and interact with your Snowflake account with all relevant prvileges and grants. |
+### Steps
 
+1. Deploy the native application, henceforth referred to as `MY_NATIVE_APP_PROJECT_VSCODE`
+
+2. Remove debug mode: `alter application MY_NATIVE_APP_PROJECT_VSCODE set debug_mode=false`
+
+3. Create an external access integration which uses the secrets and rules owned by the application:
+
+```
+create EXTERNAL ACCESS INTEGRATION EAI_TEST
+ALLOWED_NETWORK_RULES=(MY_NATIVE_APP_PROJECT_VSCODE.DATA.MY_NETWORK_RULE)
+ALLOWED_AUTHENTICATION_SECRETS=(MY_NATIVE_APP_PROJECT_VSCODE.DATA.MY_SECRET)
+enabled=true;
+```
+
+4. Grant usage on the external access integration to the application:
+
+```
+grant usage on integration EAI_TEST to application MY_NATIVE_APP_PROJECT_VSCODE;
+```
+
+5. Request that the application bind the integration and its secret to the UDFS.HELLO_WORLD function:
+
+```
+call MY_NATIVE_APP_PROJECT_VSCODE.UDFS.BIND_INTEGRATION('EAI_TEST');
+```
+
+6. Invoke the function, this should work:
+
+```
+select MY_NATIVE_APP_PROJECT_VSCODE.UDFS.HELLO_WORLD();
+```
+
+7. Request that the application revoke access to the DATA schema that the secret and network rule reside in:
+
+```
+call MY_NATIVE_APP_PROJECT_VSCODE.UDFS.REVOKE_SCHEMA_ACCESS();
+```
+
+6. Invoke the function again, this will fail:
+
+```
+select MY_NATIVE_APP_PROJECT_VSCODE.UDFS.HELLO_WORLD();
+```
+
+The error will be:
+> Schema 'MY_NATIVE_APP_PROJECT_VSCODE.DATA' does not exist or not authorized.
--- my_app/app/setup_script.sql
+++ my_app/app/setup_script.sql
@@ -6,6 +6,94 @@
 -- Extension Code
 -- Refer to https://docs.snowflake.com/en/developer-guide/native-apps/creating-setup-script for a detailed understanding of this file.
 
-CREATE OR ALTER VERSIONED SCHEMA core;
+create application role if not exists FUNCTION_CALLER;
 
--- The rest of this script is left blank for purposes of your learning and exploration.
+CREATE OR ALTER VERSIONED SCHEMA UDFS;
+
+CREATE OR REPLACE FUNCTION UDFS.HELLO_WORLD()
+RETURNS STRING
+  LANGUAGE PYTHON
+  RUNTIME_VERSION = '3.12'
+  HANDLER = 'run'
+  AS
+  $$
+def run():
+  return 'Hello, World!'
+$$;
+grant USAGE on schema UDFS to application role FUNCTION_CALLER;
+
+grant USAGE on function UDFS.HELLO_WORLD() to application role FUNCTION_CALLER;
+
+CREATE SCHEMA IF NOT EXISTS DATA;
+GRANT USAGE ON SCHEMA DATA TO APPLICATION ROLE FUNCTION_CALLER;
+
+CREATE OR REPLACE SECRET DATA.MY_SECRET TYPE = GENERIC_STRING SECRET_STRING = 'Password123';
+grant usage on secret DATA.MY_SECRET to application role FUNCTION_CALLER;
+
+CREATE OR REPLACE NETWORK RULE DATA.MY_NETWORK_RULE TYPE = HOST_PORT MODE = EGRESS VALUE_LIST = ('myapi.com');
+grant usage on network rule DATA.MY_NETWORK_RULE to application role FUNCTION_CALLER;
+
+-- this proc binds an external access integration to the HELLO_WORLD UDTF
+create or replace procedure UDFS.BIND_INTEGRATION(EAI_NAME varchar)
+   returns object
+   language javascript
+   COMMENT = $$
+   Binds an external access integration to the HELLO_WORLD UDTF.
+   $$
+   execute as owner
+as
+$$
+try{
+  var sqlText = `alter function UDFS.HELLO_WORLD()
+                set EXTERNAL_ACCESS_INTEGRATIONS = (${EAI_NAME}),
+                    SECRETS = ('MY_SECRET' = DATA.MY_SECRET)`
+    var appsResults = snowflake.createStatement( {
+        sqlText:sqlText,
+        binds:[]
+    } ).execute();
+    return {
+        "success": true,
+        "data": null
+    }
+}
+catch(e){
+   return {
+      "success": false,
+      "error": `BIND_INTEGRATION: ${String(e)}`
+   }
+}
+$$
+;
+
+grant usage on procedure UDFS.BIND_INTEGRATION(VARCHAR) to application role FUNCTION_CALLER;
+
+create or replace procedure UDFS.REVOKE_SCHEMA_ACCESS()
+   returns object
+   language javascript
+   COMMENT = $$
+   Revokes access to the internal schema
+   $$
+   execute as owner
+as
+$$
+try{
+  var sqlText = `revoke usage on schema DATA from application role FUNCTION_CALLER;`
+    var appsResults = snowflake.createStatement( {
+        sqlText:sqlText,
+        binds:[]
+    } ).execute();
+    return {
+        "success": true,
+        "data": null
+    }
+}
+catch(e){
+   return {
+      "success": false,
+      "error": `REVOKE_SCHEMA_ACCESS: ${String(e)}`
+   }
+}
+$$
+;
+
+grant usage on procedure UDFS.REVOKE_SCHEMA_ACCESS() to application role FUNCTION_CALLER;
